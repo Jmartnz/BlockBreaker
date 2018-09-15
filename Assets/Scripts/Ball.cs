ea63e68a5a5a64f594e2cfc8fa0f0f7e3971d72b
Josue Martinez Fernandez <jmartnz.designer@gmail.com>
2018-09-15 17:20:56 +0200

    First commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField] private Paddle paddle;
+    private Vector2 distance;
+
+	// Use this for initialization
+	void Start () {
+        distance = transform.position - paddle.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        LockToPaddle();
+    }
+
+    private void LockToPaddle()
+    {
+        Debug.Log("Ball Y Position: " + transform.position.y);
+        Vector2 paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = (paddlePosition + distance);
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    private int screenWidthInUnits = 16;
+    private float paddleSize;
+
+	// Use this for initialization
+	void Start () {
+        paddleSize = GetComponent<PolygonCollider2D>().bounds.size.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseXPosition = (Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mouseXPosition, (paddleSize / 2), screenWidthInUnits - (paddleSize / 2));
+
+        transform.position = paddlePosition;
+	}
+}
