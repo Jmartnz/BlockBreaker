ccd417a28a744b7315011d270678a8cb23e02f63
Josue Martinez Fernandez <jmartnz.designer@gmail.com>
2018-09-16 21:25:25 +0200

    Implemented ball launch and created first block prefabs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,22 +5,50 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 
     [SerializeField] private Paddle paddle;
-    private Vector2 distance;
+    [SerializeField] private float verticalPush;
+    [SerializeField] private float horizontalPush;
+
+    private Vector2 distanceToPaddle;
+    private bool isLocked;
 
 	// Use this for initialization
 	void Start () {
-        distance = transform.position - paddle.transform.position;
+        distanceToPaddle = transform.position - paddle.transform.position;
+        isLocked = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        LockToPaddle();
+        if (isLocked)
+        {
+            LockToPaddle();
+            // Launch ball if we do Left Click on the mouse
+            if (Input.GetMouseButtonDown(0))
+            {
+                Launch();
+            }
+        }
     }
 
     private void LockToPaddle()
     {
-        Debug.Log("Ball Y Position: " + transform.position.y);
+        // Debug.Log("Ball Y Position: " + transform.position.y); // TODO Jmartnz Fix the error with Y position decreasing
         Vector2 paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
-        transform.position = (paddlePosition + distance);
+        transform.position = paddlePosition + distanceToPaddle;
+    }
+
+    private void Launch()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalPush, verticalPush);
+        isLocked = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Paddle>())
+        {
+            Vector2 currentVelocity = GetComponent<Rigidbody2D>().velocity;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(currentVelocity.x, verticalPush); 
+        }
     }
 }
---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    [SerializeField] private int hits;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Ball>())
+        {
+            hits--;
+            if (hits <= 0) { Destroy(gameObject); }
+        }
+    }
+}
